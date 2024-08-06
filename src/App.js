b8b3938db7147ece719bedeced3207e2f0a18d7b
rendrazuriansyah <rendra.zurd04@gmail.com>
2024-08-06 22:15:07 +0700

    separating ui into components and iterating over components based on props

--- src/App.js
+++ src/App.js
@@ -5,64 +5,95 @@ function App() {
 		<>
 			<div className="card">
 				<div className="top">
-					<div className="social-buttons">
-						<button>
-							<i className="fab fa-facebook-f"></i>
-						</button>
-						<button>
-							<i className="fab fa-instagram"></i>
-						</button>
-						<button>
-							<i className="fab fa-twitter"></i>
-						</button>
-					</div>
-					<div className="social-buttons right">
-						<button>
-							<i className="fab fa-linkedin"></i>
-						</button>
-						<button>
-							<i className="fab fa-github"></i>
-						</button>
-					</div>
-					<button className="like">
-						<i className="far fa-heart"></i>
-					</button>
-					<div className="text">
-						<div className="name-wrapper">
-							<i></i>
-						</div>
-						<div className="name">Rendra Zuriansyah</div>
-						<div className="title">Fullstack Developer</div>
-					</div>
+					<Header />
+					<Identity />
 				</div>
 				<div className="bottom">
-					<div className="desc">
-						<p>
-							Nama saya <b>Rendra Zuriansyah</b>. Saya seorang
-							developer yang berpengalaman dalam pengembangan dan
-							mengintegrasikan solusi teknologi yang dapat
-							meningkatkan produktivitas dan kualitas dalam
-							pekerjaan.
-						</p>
-						<p>
-							Jika Anda memiliki pertanyaan atau memiliki proyek
-							yang ingin saya bantu, silakan hubungi saya.
-						</p>
-					</div>
-					<div className="buttons">
-						<button>
-							<i className="fas fa-envelope"></i>
-						</button>
-						<button>
-							<i className="fas fa-phone"></i>
-						</button>
-						<button>
-							<i className="fas fa-share-alt"></i>
-						</button>
-					</div>
+					<Biodata />
+					<Highlight />
 				</div>
 			</div>
 		</>
+	);
+}
+
+function SocialButton({ icon }) {
+	return (
+		<button>
+			<i className={`fab ${icon}`}></i>
+		</button>
+	);
+}
+
+function Header() {
+	return (
+		<>
+			<div className="social-buttons">
+				<SocialButton icon="fa-facebook-f" />
+				<SocialButton icon="fa-instagram" />
+				<SocialButton icon="fa-twitter" />
+			</div>
+			<div className="social-buttons right">
+				<SocialButton icon="fa-linkedin" />
+				<SocialButton icon="fa-github" />
+			</div>
+		</>
+	);
+}
+
+function Identity() {
+	return (
+		<>
+			<button className="like">
+				<i className="far fa-heart"></i>
+			</button>
+			<div className="text">
+				<div className="name-wrapper">
+					<i></i>
+				</div>
+				<div className="name">Rendra Zuriansyah</div>
+				<div className="title">Fullstack Developer</div>
+			</div>
+		</>
+	);
+}
+
+function Biodata() {
+	return (
+		<>
+			<div className="desc">
+				<p>
+					Nama saya <b>Rendra Zuriansyah</b>. Saya seorang developer
+					yang berpengalaman dalam pengembangan dan mengintegrasikan
+					solusi teknologi yang dapat meningkatkan produktivitas dan
+					kualitas dalam pekerjaan.
+				</p>
+				<p>
+					Jika Anda memiliki pertanyaan atau memiliki proyek yang
+					ingin saya bantu, silakan hubungi saya.
+				</p>
+			</div>
+		</>
+	);
+}
+
+function Badge({ text }) {
+	return (
+		<button>
+			<div className="height">
+				<i className={`fas fa-${text}`}></i>
+			</div>
+		</button>
+	);
+}
+
+function Highlight() {
+	return (
+		<div className="buttons">
+			<Badge text="envelope" />
+			<Badge text="phone" />
+			<Badge text="share-alt" />
+		</div>
 	);
 }
 
